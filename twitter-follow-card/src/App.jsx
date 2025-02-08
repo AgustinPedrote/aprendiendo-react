a8e0b373e64ebfb598992fcfe2a61a9f94a44cf6
agucadiz <agu.cadiz@hotmail.com>
2025-02-08 10:13:28 +0100

    Botón empezar en 'Seguir'

--- twitter-follow-card/src/App.jsx
+++ twitter-follow-card/src/App.jsx
@@ -4,10 +4,10 @@ import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 export function App() {
   return (
     <section className='App'>
-      <TwitterFollowCard initialIsFollowing={true} userName="socrates" name="Socrates Liliet" />
-      <TwitterFollowCard initialIsFollowing={true} userName="topuria" name="Max Topuria" />
-      <TwitterFollowCard initialIsFollowing={true} userName="max" name="Max Simpson" />
-      <TwitterFollowCard initialIsFollowing={true} userName="jhonS" name="Jhon Smith" />
+      <TwitterFollowCard initialIsFollowing={false} userName="socrates" name="Socrates Liliet" />
+      <TwitterFollowCard initialIsFollowing={false} userName="topuria" name="Max Topuria" />
+      <TwitterFollowCard initialIsFollowing={false} userName="max" name="Max Simpson" />
+      <TwitterFollowCard initialIsFollowing={false} userName="jhonS" name="Jhon Smith" />
     </section>
   )
 }
